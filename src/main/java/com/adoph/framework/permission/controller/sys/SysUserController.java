b18226fd04071d785b94b2e25744118644932b6d
Adoph <tangqiandong@sm.vvip-u.com>
2017-10-17 18:03:44 +0800

    1.后台布局（待完善）

--- src/main/java/com/adoph/framework/permission/controller/sys/SysUserController.java
+++ src/main/java/com/adoph/framework/permission/controller/sys/SysUserController.java
@@ -1,12 +1,19 @@
 package com.adoph.framework.permission.controller.sys;
 
+import com.adoph.framework.permission.pojo.SysUser;
 import com.adoph.framework.permission.service.sys.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.List;
 
 /**
  * 系统用户管理控制层
@@ -22,10 +29,19 @@ public class SysUserController {
     @Autowired
     private SysUserService sysUserService;
 
-    public void querySysUserList() {
+    @GetMapping("index")
+    public String index() {
+        return "/admin/user_list";
+    }
+
+    @RequestMapping("list")
+    @ResponseBody
+    public void list() {
         Sort sort = new Sort(Sort.Direction.ASC, "id");
-        Pageable pageable = new PageRequest(1, 15, sort);
-        sysUserService.findAll("admin", pageable);
+        Pageable pageable = new PageRequest(0, 15, sort);
+        Page<SysUser> page = sysUserService.findAll("admin", pageable);
+        List<SysUser> list = page.getContent();
+//        return object;
     }
 
 }
