1bbae107551601ecd719b2ca4208b2f13f7ea448
Adoph <tangqiandong@sm.vvip-u.com>
2018-03-14 12:04:46 +0800

    1.hibernate测试分析完成

--- src/main/java/com/adoph/framework/test/HibernateTest.java
+++ src/main/java/com/adoph/framework/test/HibernateTest.java
@@ -19,7 +19,7 @@ import java.util.Objects;
  * save、update、saveOrUpdate、persist、merge等方法详细测试分析
  * 三种状态：
  * transient(瞬时状态)：刚new出来的对象，还没被保存到数据库，并且没有被持久化
- * persistent(持久化状态)，
+ * persistent(持久化状态)
  * detached(游离状态)
  *
  * @author Adoph
@@ -49,6 +49,57 @@ public class HibernateTest {
         System.out.println(u1);
         System.out.println("两次获取的对象比较结果是否相同：" + Objects.equals(u1, u2));
         System.out.println("两次获取的对象比较结果是否相同：" + Objects.equals(u1, list.get(0)));
+    }
+
+    /**
+     * 1.new出来的对象save过后，由游离状态（detached）变为持久状态（persistent）
+     */
+    @Test
+    public void testSave1() {
+        Session session1 = sessionFactory.openSession();
+        session1.beginTransaction();//开启事务
+        User u3 = new User();
+        u3.setUserName("u5");
+        u3.setPassword("7762dc5d78cb8366208b0c05b6fb14ea");
+        u3.setCreatedBy(1L);
+        session1.save(u3);
+        u3.setUserName("uef");
+        session1.getTransaction().commit();//提交事务
+    }
+
+    /**
+     * 1.主键的生成策略:
+     *
+     * @see javax.persistence.GenerationType
+     */
+    @Test
+    public void testSave2() {
+        Session session1 = sessionFactory.openSession();
+        session1.beginTransaction();//开启事务
+        User u3 = new User();
+        u3.setId(40L);
+        u3.setUserName("like");
+        u3.setPassword("7762dc5d78cb8366208b0c05b6fb14ea");
+        u3.setCreatedBy(1L);
+        session1.save(u3);
+        session1.getTransaction().commit();//提交事务
+    }
+
+    /**
+     * 1.persist类似save方法
+     */
+    @Test
+    public void testPersist() {
+        Session session1 = sessionFactory.openSession();
+        session1.beginTransaction();//开启事务
+        User u3 = new User();
+//        u3.setId(33L);
+        u3.setUserName("bus");
+        u3.setPassword("7762dc5d78cb8366208b0c05b6fb14ea");
+        u3.setCreatedBy(1L);
+        session1.persist(u3);
+        session1.getTransaction().commit();//提交事务
+        session1.close();
     }
 
     /**
@@ -86,6 +137,9 @@ public class HibernateTest {
         session1.getTransaction().commit();//提交事务
     }
 
+    /**
+     * 1.new出来的对象，update必须设置主键值，并且数据库存在该id
+     */
     @Test
     public void testUpdate3() {
         Session session1 = sessionFactory.openSession();
@@ -97,5 +151,28 @@ public class HibernateTest {
         u3.setCreatedBy(1L);
         session1.update(u3);
         session1.getTransaction().commit();//提交事务
+    }
+
+    /**
+     * 1.new出来的对象，merge过后状态没有变化，还是处于游离状态
+     * 2.merge的具体实现，是将需要merge的对象copy一个，然后再将copy对象(会被持久化到session中)进行处理
+     * 3.merge先执行select，再执行insert或者update
+     * 4.如果设置了id，先执行select，select未找到该对象执行insert，反之执行update
+     */
+    @Test
+    public void testMerge() {
+        Session session1 = sessionFactory.openSession();
+        session1.beginTransaction();//开启事务
+        User u3 = new User();
+//        u3.setId(33L);
+        u3.setId(40L);//数据库未存在的id值
+        u3.setUserName("water");
+        u3.setPassword("7762dc5d78cb8366208b0c05b6fb14ea");
+        u3.setCreatedBy(1L);
+        session1.merge(u3);
+        u3.setUserName("qs");//此行代码无效，u3依然处于游离状态
+        session1.getTransaction().commit();//提交事务
+//        User u5 = session1.get(User.class, 33L);
+//        System.out.println(u5);
     }
 }
--- src/main/java/com/adoph/framework/test/pojo/User.java
+++ src/main/java/com/adoph/framework/test/pojo/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
      */
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     /**
